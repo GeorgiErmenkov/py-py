cd8b90dbd50508db5f0a79ce9f0b7a4cc8c80e04
georgi <germenkov@gmail.com>
2021-01-26 15:01:21 +0200

    3 new solutions

---
+++ one-file-tasks/question53.py
@@ -0,0 +1,16 @@
+#Question 53
+#Define a class named Rectangle which can be constructed by a length and width. The Rectangle class 
+# has a method which can compute the area.
+#Hints:
+#Use def methodName(self) to define a method.
+
+class Rectangle(object):
+    def __init__(self, length, width):
+        self.length=length
+        self.width=width
+    
+    def area(self):
+        return self.length*self.width
+
+Rect1=Rectangle(5, 10)
+print(Rect1.area())
